d49dead38407d2420bb7d69abdb0164b1fb8b196
zviriv <41854469+zviriv@users.noreply.github.com>
2023-07-04 18:41:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.all('/', (req, res) => {
     console.log(req);
     
