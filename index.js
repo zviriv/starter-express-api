d05f0c9ecae9cffcc5630e4cd4278854f1f84001
zviriv <41854469+zviriv@users.noreply.github.com>
2023-07-04 18:29:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 app.all('/', (req, res) => {
-    console.log("Just got a request!");
+    console.log(req);
+    
     res.send('Yo!');
 });
 app.listen(process.env.PORT || 3000);
