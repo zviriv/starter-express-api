b37e026833b968c9ea171d1808716d77a9ccce86
zviriv <41854469+zviriv@users.noreply.github.com>
2023-07-04 19:18:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
