4a8564414e600e76f59eb75e01474cd648002d09
zviriv <41854469+zviriv@users.noreply.github.com>
2023-07-04 18:43:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
+}); 
 
 app.all('/', (req, res) => {
     console.log(req);
